8e8eb44d4710fe3c2427d875fd16649a6f731c94
justkresge <justkresge@gmail.com>
2013-09-03 12:17:46 -0400

    New Menu Structure

--- CheckPoint2/src/com/hooper/checkpoint2/MainActivity.java
+++ CheckPoint2/src/com/hooper/checkpoint2/MainActivity.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
-import android.widget.Button;
+import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	ImageButton playbtn;
 	ImageButton scoresbtn;
@@ -26,66 +26,44 @@ public class MainActivity extends Activity {
 		aboutbtn = (ImageButton) findViewById(R.id.about_btn);
 		exitbtn = (ImageButton) findViewById(R.id.exit_btn);
 
-		// Tell the Button Clicks
+		playbtn.setOnClickListener(this);
+		scoresbtn.setOnClickListener(this);
+		settingsbtn.setOnClickListener(this);
+		aboutbtn.setOnClickListener(this);
+		exitbtn.setOnClickListener(this);
+	}
+		
+		@Override
+		public void onClick(View v) {
 
-		playbtn.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-
+			if(v.getId() == R.id.play_btn) {
 				Intent openPlayAct = new Intent(
 						"com.hooper.checkpoint2.PlayActivity");
-				startActivity(openPlayAct);
-
+				startActivity(openPlayAct);	
 			}
-		});
-
-		scoresbtn.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
+			if(v.getId() == R.id.scores_btn) {
 				Intent openPlayAct = new Intent(
 						"com.hooper.checkpoint2.ScoresActivity");
-				startActivity(openPlayAct);
+				startActivity(openPlayAct);	
 			}
-		});
-
-		settingsbtn.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Intent openPlayAct = new Intent(
-						"com.hooper.checkpoint2.SettingsActivity");
-				startActivity(openPlayAct);
-
-			}
-		});
-		
-		aboutbtn.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
+			if(v.getId() == R.id.about_btn) {
 				Intent openPlayAct = new Intent(
 						"com.hooper.checkpoint2.AboutActivity");
-				startActivity(openPlayAct);
-
+				startActivity(openPlayAct);	
 			}
-		});
-		
-		exitbtn.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
+			if(v.getId() == R.id.settings_btn) {
+				Intent openPlayAct = new Intent(
+						"com.hooper.checkpoint2.SettingsActivity");
+				startActivity(openPlayAct);	
+			}
+			if(v.getId() == R.id.exit_btn) {
 				Intent openPlayAct = new Intent(
 						"com.hooper.checkpoint2.ExitActivity");
-				startActivity(openPlayAct);
-
+				startActivity(openPlayAct);	
 			}
-		});
+			
 
-	}
-
+		}
 	
-	
+	};
 
-}
